6afb28870a178f0ba69c178a72aef7667e242d44
Mohammad Baqer <mtbaqer@gmail.com>
2019-12-04 20:38:08 -0500

    made the backend serve the frontend

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
+app.use(express.static(path.join(__dirname, "client/build")));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
