83eed030971375c7a8628766f78367375b981b39
Mohammad Baqer <mtbaqer@gmail.com>
2019-12-07 21:45:10 -0500

    added the ability to select a file

--- client/src/components/UploadDialog.tsx
+++ client/src/components/UploadDialog.tsx
@@ -2,21 +2,39 @@ import * as React from "react";
 import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from "@material-ui/core";
 import styled from "styled-components";
 
+const Input = styled.input`
+  display: none;
+`;
+
 export interface UploadDialogProps {
   open: boolean;
   handleClose: Function;
 }
 
-class UploadDialog extends React.Component<UploadDialogProps, any> {
+export interface UploadDialogState {
+  selectedFile: File | null;
+}
+
+export default class UploadDialog extends React.Component<UploadDialogProps, UploadDialogState> {
+  state: UploadDialogState = {
+    selectedFile: null
+  };
+  handleFileSelected = (selectedFile: File) => {
+    this.setState({ selectedFile: selectedFile });
+  };
+
   render() {
     return (
       <Dialog fullWidth maxWidth="xs" onClose={() => this.props.handleClose()} open={this.props.open}>
         <DialogTitle>Upload CSV File</DialogTitle>
         <DialogContent>
-          <DialogContentText>No file selected</DialogContentText>
-          <Button onClick={() => this.props.handleClose()} color="primary">
-            Select File
-          </Button>
+          <DialogContentText>{this.state.selectedFile ? this.state.selectedFile.name : "No file selected"}</DialogContentText>
+          <Input accept=".csv" id="uploadInput" type="file" onChange={event => this.handleFileSelected(event.target.files![0])} />
+          <label htmlFor="uploadInput">
+            <Button color="primary" component="span">
+              Select File
+            </Button>
+          </label>
         </DialogContent>
         <DialogActions>
           <Button onClick={() => this.props.handleClose()} color="primary">
@@ -28,5 +46,3 @@ class UploadDialog extends React.Component<UploadDialogProps, any> {
     );
   }
 }
-
-export default UploadDialog;
