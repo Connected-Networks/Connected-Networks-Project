1c56c00df881ecf341dd4f0bf9686c14d82f7799
AaronPMichael <michaeap@rose-hulman.edu>
2019-12-09 18:14:06 -0500

    converted to Typescript class, added point for frontend connection

--- backend-processing.ts
+++ backend-processing.ts
@@ -1,4 +1,3 @@
-
 const mysql = require('mysql')
 const papa = require('Papaparse')
 const con = mysql.createConnection({
@@ -14,26 +13,34 @@ con.connect((err)=>{
     }
     console.log('Connection established')
 })
-let input:string = ",,,,,,Periodic Updates for 2019 (Any Job Changes?),,,,\nName,Position,Employment Term,Current Employer,Position,Hyperlink Url,Q1 2019,Q2 2019,Q3 2019,Q4 2019,Comments\nStephen Schwarzman,Managing Investor,June-Dec 2017,The Blackstone Group,Co founder,https://www.linkedin.com/in/stephenschwarzman/,,\"Chairman, The Blackstone Group\",,,Would hire again at any point"
-processRawCSV(input)
+//let input:string = ",,,,,,Periodic Updates for 2019 (Any Job Changes?),,,,\nName,Position,Employment Term,Current Employer,Position,Hyperlink Url,Q1 2019,Q2 2019,Q3 2019,Q4 2019,Comments\nStephen Schwarzman,Managing Investor,June-Dec 2017,The Blackstone Group,Co founder,https://www.linkedin.com/in/stephenschwarzman/,,\"Chairman, The Blackstone Group\",,,Would hire again at any point"
+//processRawCSV(input)
+
+interface DisplayPerson{
+    name:String
+    comment:String
+}
+export default class backendProcessing{
 
 
-function processRawCSV(data:string){
+
+
+processRawCSV(data:string){
 
     //First line is commented and ignored
     //Second line is treated as header
     var results = papa.parse("#"+data,{header:true,comments:"#"})
     results.data.forEach(element => {
-        let pcall = createCallForCSV(element)
+        let pcall = this.createCallForCSV(element);
     });
 }
 
-function retrievePeopleFromDatabase(){
+retrievePeopleFromDatabase(){
     con.query('CALL <fill in later>',(err,rows) => {
         if (err) throw err
         let response:DisplayPerson[]
         rows[0].foreach(row=>{
-            let dp=new DisplayPerson()
+            let dp:DisplayPerson;
             dp.name = row.name;
             //fill in lines
             dp.comment = row.comment;
@@ -41,10 +48,12 @@ function retrievePeopleFromDatabase(){
         })
     })
 }
-//Papaparse gives maps with fields: Name,Position, Employment Term, etc.
-function createCallForCSV(person){
+//Papaparse gives (maps?) with fields: Name,Position, Employment Term, etc.
+createCallForCSV(person){
+    let name = person.Name
+    let position = person.Position
+    let term = person['Current Employer']
     let call = 'CALL <fill in later>'
-    console.log(person.Name)
 
 
 }
@@ -54,10 +63,5 @@ function createCallForCSV(person){
 
 
 
-class DisplayPerson{
-    public name:String
-    public comment:String
-
 
 }
-
--- server.ts
+++ server.ts
@@ -1,3 +1,5 @@
+import backendProcessing from "./backend-processing";
+
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -9,3 +11,13 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
+
+app.post("/csv",(req,res)=>{
+  const data = req.body.data
+  backendProcessing.processRawCSV(data)
+
+
+
+
+
+})
