24aa17ca9efb4ebe54eed74c1ebdd9f1efb9e902
Aaron Michael <apmichael9@gmail.com>
2019-12-09 17:44:52 -0500

    more data processing and packages

--- backend-processing.ts
+++ backend-processing.ts
@@ -1,5 +1,6 @@
 
 const mysql = require('mysql')
+const papa = require('Papaparse')
 const con = mysql.createConnection({
     //todo: replace later
     host: 'localhost',
@@ -13,111 +14,50 @@ con.connect((err)=>{
     }
     console.log('Connection established')
 })
-let input:String = ",,,,,,Periodic Updates for 2019 (Any Job Changes?),,,,\nName,Position,Employment Term,Current Employer,Position,Hyperlink Url,Q1 2019,Q2 2019,Q3 2019,Q4 2019,Comments\nStephen Schwarzman,Managing Investor,June-Dec 2017,The Blackstone Group,Co founder,https://www.linkedin.com/in/stephenschwarzman/,,\"Chairman, The Blackstone Group\",,,Would hire again at any point"
+let input:string = ",,,,,,Periodic Updates for 2019 (Any Job Changes?),,,,\nName,Position,Employment Term,Current Employer,Position,Hyperlink Url,Q1 2019,Q2 2019,Q3 2019,Q4 2019,Comments\nStephen Schwarzman,Managing Investor,June-Dec 2017,The Blackstone Group,Co founder,https://www.linkedin.com/in/stephenschwarzman/,,\"Chairman, The Blackstone Group\",,,Would hire again at any point"
 processRawCSV(input)
 
 
-function processRawCSV(data:String){
-    let dataLines:String[] = extractNonheaderLines(data)
-    let people:CSVPerson[] = new CSVPerson[dataLines.length]
-    dataLines.forEach(element => {
-        let person = new CSVPerson(element)
-        people.push(person)
-        console.log(person.basicDetails())
-        let q = person.procedureString()
-        con.query("CALL "+q,function(err){
-            if (err)
-                console.log('Error sending data to database')
-        })
+function processRawCSV(data:string){
+
+    //First line is commented and ignored
+    //Second line is treated as header
+    var results = papa.parse("#"+data,{header:true,comments:"#"})
+    results.data.forEach(element => {
+        let pcall = createCallForCSV(element)
     });
 }
-function extractNonheaderLines(data:String):String[]{
-    let lineSeparator = '/n'
-    let headerLines = 2;
-    let lines:String[] = data.split(lineSeparator);
-    lines = lines.slice(2)
-    return lines;
+
+function retrievePeopleFromDatabase(){
+    con.query('CALL <fill in later>',(err,rows) => {
+        if (err) throw err
+        let response:DisplayPerson[]
+        rows[0].foreach(row=>{
+            let dp=new DisplayPerson()
+            dp.name = row.name;
+            //fill in lines
+            dp.comment = row.comment;
+            response.push(dp)
+        })
+    })
 }
-function retrievePeopleFromDatabase():DisplayPerson[]{
+//Papaparse gives maps with fields: Name,Position, Employment Term, etc.
+function createCallForCSV(person){
+    let call = 'CALL <fill in later>'
+    console.log(person.Name)
 
-
-
-
-
-    return null
-}
-
-
-
-
-
-
-
-class CSVPerson{
-    toProcess:String
-
-    name:String
-    position:String
-    employmentTerm:String
-    currentEmployer:String
-    //represents past relation
-    pastPosition:String
-    link:String
-    update1:String
-    update2:String
-    update3:String
-    update4:String
-    comment:String
-
-
-    public constructor(data:String){
-        this.toProcess = data
-        this.ExtractFields()
-    }
-    private ExtractFields(){
-        this.name = this.getNextValue()
-        this.position = this.getNextValue()
-        this.employmentTerm = this.getNextValue()
-        this.currentEmployer = this.getNextValue()
-        this.pastPosition = this.getNextValue()
-        this.link = this.getNextValue()
-        this.update1 = this.getNextValue()
-        this.update2 = this.getNextValue()
-        this.update3 = this.getNextValue()
-        this.update4 = this.getNextValue()
-        this.comment = this.getNextValue()
-    }
-    private getNextValue():String{
-        let boxSeparator = ','
-        let nextValue = ""
-        let inQuotes = false
-        let done = false
-        while (this.toProcess.length>0 && !done){
-            let c = this.toProcess.slice(0,1)
-            this.toProcess = this.toProcess.slice(1)
-            if (c=='"')
-                inQuotes = !inQuotes
-            else if (c==boxSeparator && !inQuotes)
-                done = true
-            else
-                nextValue = nextValue + c
-        }
-        return nextValue
-    }
-    public basicDetails():String{
-        let details = this.name + " : " + this.position
-        if (this.comment.length>0)
-            details += "("+this.comment+")"
-        return details
-    }
-    public procedureString():String{
-        //TODO: fill in method to match procedure syntax
-        return ""
-    }
 
 }
+
+
+
+
+
 
 class DisplayPerson{
+    public name:String
+    public comment:String
+
 
 }
 
