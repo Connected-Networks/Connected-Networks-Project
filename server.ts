7da2fe514171338a16838c9197a402526314c90a
Mohammad Baqer <mtbaqer@gmail.com>
2019-12-10 14:01:48 -0500

    moved the basic CSV parsing code to server.ts until resolve the imports problems

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
-const backendProcessing = require("./backend-processing.ts");
-
 const express = require("express");
 const path = require("path");
+const papa = require("Papaparse");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,17 +10,32 @@ app.use(express.static(path.join(__dirname, "client/build")));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.post("/csv", (req, res) => {
-  const data = req;
-  console.log(req.body);
   try {
-    let be = new backendProcessing();
-    be.processRawCSV(data);
+    processRawCSV(req.body.data);
     res.sendStatus(200);
   } catch (error) {
-    console.log("error");
+    console.log(error);
     res.sendStatus(500);
   }
 });
+
+function processRawCSV(data: string) {
+  //First line is commented and ignored
+  //Second line is treated as header
+  var results = papa.parse("#" + data, { header: true, comments: "#" });
+  results.data.forEach(element => {
+    createCallForCSV(element);
+  });
+}
+
+function createCallForCSV(entry) {
+  let name = entry.Name;
+  let position = entry.Position;
+  let employer = entry["Current Employer"];
+  let term = entry["Employment Term"];
+  let call = `Received ${name} at ${employer} doing ${position} during ${term}`;
+  console.log(call);
+}
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
