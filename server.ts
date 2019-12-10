fe45ceffc8f103b23aa901a8466092bda214a048
Mohammad Baqer <mtbaqer@gmail.com>
2019-12-09 19:46:52 -0500

    made API endpoints able to read request body

--- server.ts
+++ server.ts
@@ -5,15 +5,14 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "client/build")));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
-
 app.post("/csv", (req, res) => {
-  const data = req.body.data;
+  const data = req;
+  console.log(req.body);
   try {
     let be = new backendProcessing();
     be.processRawCSV(data);
@@ -22,4 +21,8 @@ app.post("/csv", (req, res) => {
     console.log("error");
     res.sendStatus(500);
   }
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
