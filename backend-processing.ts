017f748b5b60768cc60c4fcd317017e963c42e6b
AaronPMichael <michaeap@rose-hulman.edu>
2019-12-09 19:09:10 -0500

    changed away from database code for demo

--- backend-processing.ts
+++ backend-processing.ts
@@ -1,18 +1,18 @@
-const mysql = require("mysql");
-const papa = require("Papaparse");
-const con = mysql.createConnection({
-  //todo: replace later
-  host: "localhost",
-  user: "user",
-  password: "password"
-});
-con.connect(err => {
-  if (err) {
-    console.log("Error connecting to database");
-    return;
-  }
-  console.log("Connection established");
-});
+// const mysql = require("mysql");
+ const papa = require("Papaparse");
+// const con = mysql.createConnection({
+//   //todo: replace later
+//   host: "localhost",
+//   user: "user",
+//   password: "password"
+// });
+// con.connect(err => {
+//   if (err) {
+//     console.log("Error connecting to database");
+//     return;
+//   }
+//  console.log("Connection established");
+//});
 //let input:string = ",,,,,,Periodic Updates for 2019 (Any Job Changes?),,,,\nName,Position,Employment Term,Current Employer,Position,Hyperlink Url,Q1 2019,Q2 2019,Q3 2019,Q4 2019,Comments\nStephen Schwarzman,Managing Investor,June-Dec 2017,The Blackstone Group,Co founder,https://www.linkedin.com/in/stephenschwarzman/,,\"Chairman, The Blackstone Group\",,,Would hire again at any point"
 //processRawCSV(input)
 
@@ -32,23 +32,25 @@ export default class BackendProcessing {
   }
 
   retrievePeopleFromDatabase() {
-    con.query("CALL <fill in later>", (err, rows) => {
-      if (err) throw err;
-      let response: DisplayPerson[];
-      rows[0].foreach(row => {
-        let dp: DisplayPerson;
-        dp.name = row.name;
-        //fill in lines
-        dp.comment = row.comment;
-        response.push(dp);
-      });
-    });
+    // con.query("CALL <fill in later>", (err, rows) => {
+    //   if (err) throw err;
+    //   let response: DisplayPerson[];
+    //   rows[0].foreach(row => {
+    //     let dp: DisplayPerson;
+    //     dp.name = row.name;
+    //     //fill in lines
+    //     dp.comment = row.comment;
+    //     response.push(dp);
+    //   });
+    // });
   }
   //Papaparse gives (maps?) with fields: Name,Position, Employment Term, etc.
-  createCallForCSV(person) {
-    let name = person.Name;
-    let position = person.Position;
-    let term = person["Current Employer"];
-    let call = "CALL <fill in later>";
+  createCallForCSV(entry) {
+    let name = entry.Name;
+    let position = entry.Position;
+    let employer = entry["Current Employer"];
+    let term = entry["Employment Term"]
+    let call = `Recieved ${name} at ${employer} doing ${position} during ${term}`;
+    console.log(call);
   }
 }
