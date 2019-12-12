da01d151c73101aec968f837abea920a77ba9f72
MongoSP <48965695+MongoSP@users.noreply.github.com>
2019-12-12 17:52:26 -0500

    Added primary and foreign keys

--- sqlQueries/ConstructTables.sql
+++ sqlQueries/ConstructTables.sql
@@ -1,30 +1,37 @@
 CREATE TABLE Individuals (
-	IndividualID int,
+	IndividualID int NOT NULL AUTO_INCREMENT,
 	IndividualName varchar(255),
 	CurrentCompany varchar(255),
 	CurrentPosition varchar(255),
 	LinkedInUrl varchar(500),
-	Comments varchar(500)
+	Comments varchar(500),
+    PRIMARY KEY (IndividualID)
 );
 
 CREATE TABLE Companies (
-	CompanyID int,
-	CompanyName varchar(255)
+	CompanyID int NOT NULL AUTO_INCREMENT,
+	CompanyName varchar(255),
+    PRIMARY KEY (CompanyID)
 );
 CREATE TABLE EmployeeHistory (
-	IndividualID int,
-	CompanyID int,
+	IndividualID int NOT NULL,
+	CompanyID int NOT NULL,
 	PositionName varchar(255),
 	StartDate date,
-	EndDate date
+	EndDate date,
+    FOREIGN KEY (IndividualID) REFERENCES Individuals (IndividualID),
+    FOREIGN KEY (CompanyID) REFERENCES Companies (CompanyID)
 );
 
 CREATE TABLE Funds (
-	FundID int,
-	FundName varchar(255)
+	FundID int NOT NULL AUTO_INCREMENT,
+	FundName varchar(255),
+    PRIMARY KEY (FundID)
 );
 
 CREATE TABLE FundCompanies (
-	FundID int,
-	CompanyID int
+	FundID int NOT NULL,
+	CompanyID int NOT NULL,
+    FOREIGN KEY (FundID) REFERENCES Funds(FundID),
+    FOREIGN KEY (CompanyID) REFERENCES Companies (CompanyID)
 );
--- sqlQueries/DropAllTables.sql
+++ sqlQueries/DropAllTables.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS fundcompanies;
 
 DROP TABLE IF EXISTS employeehistory;
-
-DROP TABLE IF EXISTS fundcompanies;
 
 DROP TABLE IF EXISTS funds;
 
 DROP TABLE IF EXISTS individuals;
+
+DROP TABLE IF EXISTS companies;
