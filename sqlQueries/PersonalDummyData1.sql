2db92d90c470fd890182f803e9b16fae99925e7e
MongoSP <48965695+MongoSP@users.noreply.github.com>
2019-12-15 12:28:00 -0500

    Added more tests in Personal Dummy Data. These tests are meant to be used for determining the code for creating a view for seeing an individuals most recent employment history, hence the out of order history entry of this data.

--- sqlQueries/PersonalDummyData1.sql
+++ sqlQueries/PersonalDummyData1.sql
@@ -45,3 +45,42 @@ INSERT INTO Individuals (
 	Comments
 ) VALUES (1, "Brain M", "Developer", "individualsUrl",  "No Comment");
 */
+
+/* 12/15/19 For Branch #20: 
+	Data for this is meant to test GROUP BY and ORDER BY in
+    EmployeeHistory, so that it can be used for obtaining the 
+    most recent employee positions.
+*/
+
+INSERT INTO Individuals (
+	IndividualName,
+    OriginalPostion,
+	LinkedInUrl,
+	Comments
+) VALUES ("Riley M", "Originally a Programmer", "individualsUrl",  "No Comment");
+
+INSERT INTO EmployeeHistory  (
+	IndividualID,
+    CompanyID,	
+    PositionName,	
+    StartDate,
+    EndDate
+) VALUES (3,3, "Oldest Bethesda Position",  '2015-01-30', '2015-12-05');
+
+INSERT INTO EmployeeHistory  (
+	IndividualID,
+    CompanyID,	
+    PositionName,	
+    StartDate,
+    EndDate
+) VALUES (3,3, "Most Recent/Current Bethesda Position",  '2019-01-30', '2019-12-05');
+
+INSERT INTO EmployeeHistory  (
+	IndividualID,
+    CompanyID,	
+    PositionName,	
+    StartDate,
+    EndDate
+) VALUES (3,3, "Middle Years Bethesda Position",  '2016-01-30', '2016-12-05');
+
+
