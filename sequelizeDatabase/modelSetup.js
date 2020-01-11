1c802d717dffd91b7799679f39558c9960573a8c
MongoSP <48965695+MongoSP@users.noreply.github.com>
2020-01-11 17:15:52 -0500

    File for Sequelize Models.

---
+++ sequelizeDatabase/modelSetup.js
@@ -0,0 +1,127 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('localDatabaseSean','root','ConnectedNetwork1',{
+    host: 'localhost',
+    dialect: 'mysql',
+    define: {
+      freezeTableName: true
+    }
+  });
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+  const Individuals = sequelize.define('individuals',{
+    IndividualID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey : true
+    },
+    IndividualName: {
+      type: Sequelize.STRING
+    },
+    OriginalPostion: {
+      type: Sequelize.STRING
+    },
+    LinkedInUrl : {
+      type: Sequelize.STRING(500)
+    },
+    Comments : {
+      type: Sequelize.STRING(500)
+    }
+  }, {
+    //options:
+    timestamps: false
+  });
+  
+  const Companies = sequelize.define('companies',{
+    CompanyID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey : true
+    },
+    CompanyName: {
+      type: Sequelize.STRING
+    }
+  }, {
+    //Options: (These options relate to some Sequelize features.)
+    timestamps: false
+  });
+  
+  const Funds = sequelize.define('funds',{
+    FundID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true
+      },
+    FundName: {
+      type: Sequelize.STRING
+    }
+  }, {
+    //Options
+    timestamps: false
+  });
+
+  const EmployeeHistory = sequelize.define('employeehistory',{
+    HistoryID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    IndividualID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: Individuals,
+        key: 'IndividualID'
+      }
+    },
+    CompanyID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+          model: Companies,
+          key: 'CompanyID'
+      }
+    },
+    PositionName: {
+      type: Sequelize.STRING
+    },
+    StartDate: {
+      type: Sequelize.DATEONLY
+    },
+    EndDate:{
+      type: Sequelize.DATEONLY
+    }
+  } ,{
+    timestamps: false
+  });
+
+  const FundCompany = sequelize.define('fundcompanies',{
+      FundCompanyID: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      FundID: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: Funds,
+            key: 'FundID'
+        }
+      },
+      CompanyID: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: Companies,
+            key: 'CompanyID'
+        }
+      },
+  })
