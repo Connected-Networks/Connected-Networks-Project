cdfbda89d5ee9b8bf367e071a116a42bc7dbd1d6
Aaron Michael <michaeap@rose-hulman.edi>
2020-01-12 14:52:24 -0500

    added changes that weren't added during the last commit.

--- code_testing.ts
+++ code_testing.ts
@@ -9,14 +9,14 @@ function test_connect_to_database(){
     let input:string = ",,,,,,Periodic Updates for 2019 (Any Job Changes?),,,,\nName,Position,Employment Term,Current Employer,Position2,Hyperlink Url,Q1 2019,Q2 2019,Q3 2019,Q4 2019,Comments\nStephen Schwarzman,Managing Investor,June-Dec 2017,The Blackstone Group,Co founder,https://www.linkedin.com/in/stephenschwarzman/,,\"Chairman, The Blackstone Group\",,,Would hire again at any point"
     let be = new BackendProcessing()
     be.processRawCSV(input)
-    be.end_connection()
+    //be.end_connection()
 }
 
 function test_date_parser(){
     let input = "June-December 2017"
     let be = new BackendProcessing()
     let answer = be.convertDates(input)
-    be.end_connection()
+    //be.end_connection()
     console.log(answer)
 }
 
@@ -24,7 +24,7 @@ function test_retrieve_from_database(){
     let be = new BackendProcessing()
     be.retrievePeopleFromDatabase().then(result=>{
     console.log("result: "+result)
-    be.end_connection()
+    //be.end_connection()
     })
 }
 
@@ -67,4 +67,11 @@ function delay(ms: number)
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-test_continuous_connection()
+function call_retrieve_people(){
+    let be = new BackendProcessing()
+    be.retrievePeopleFromDatabase().then(result=>{
+        console.log(result)
+    })
+}
+
+call_retrieve_people()
