ef3422a27b8ff84e343e68f3d246d58879813bc2
MongoSP <48965695+MongoSP@users.noreply.github.com>
2020-01-16 16:08:52 -0500

    Created the functions for getting everything from each of the tables. Will do a local test in a temporary file to ensure they work as intended.

--- sequelizeDatabase/sequelFunctions.js
+++ sequelizeDatabase/sequelFunctions.js
@@ -11,3 +11,39 @@ insertPerson = (IndividualName, OriginalPosition, LinkedInUrl, Comments) => {
         console.log('Individual Created: ',user);
     }).catch(err => console.error('Error in insertPerson', err));
 }
+
+//---------------FindAll: Get all data from the table --------------//
+
+getAllIndividuals = () => {
+    models.Individuals.findAll().then((individuals) => {
+        return individuals;
+    });
+};
+getAllCompanies = () => {
+    models.Companies.findAll().then((companies) => {
+        return companies;
+    });
+};
+getAllFunds = () => {
+    models.Funds.findAll().then((funds) => {
+        return funds;
+    });
+};
+getAllEmployeeHistory = () => {
+    models.EmployeeHistory.findAll().then((employeeHistory) => {
+        return employeeHistory;
+    });
+};
+getAllFundCompany = () => {
+    models.FundCompany.findAll().then((fundCompany) => {
+        return fundCompany;
+    });
+};
+
+module.exports = {
+    getAllIndividuals,
+    getAllCompanies,
+    getAllFunds,
+    getAllEmployeeHistory,
+    getAllFundCompany
+}
