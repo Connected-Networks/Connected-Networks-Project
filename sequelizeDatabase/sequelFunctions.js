0224949f727fb30f8296eb493eaba3dbb74815bd
MongoSP <48965695+MongoSP@users.noreply.github.com>
2020-01-20 18:01:33 -0500

    Added and tested function for modifying an individual. This function does not change the individual's ID.

--- sequelizeDatabase/sequelFunctions.js
+++ sequelizeDatabase/sequelFunctions.js
@@ -89,6 +89,23 @@ getIndividualCurrentEmployement = (IndividualID) => {
     })
 }
 
+//---------------Modify Existing Data Functions----------//
+modifyIndividual = (IndividualID, newName, newPosition, newUrl, newComments) => {
+    return models.Individuals.findOne({
+        where: {
+            IndividualID: IndividualID
+        }
+    }).then(individual => {
+        individual.update({
+            IndividualName: newName,
+            OriginalPostion: newPosition,
+            LinkedInUrl: newUrl,
+            Comments: newComments
+        })
+        return individual;
+    }).catch(err => console.error(err));
+}
+
 module.exports = {
     getAllIndividuals,
     getAllCompanies,
@@ -100,5 +117,6 @@ module.exports = {
     insertEmployeeHistory,
     insertFromCsvLine,
     getIndividualEmployeeHistory,
-    getIndividualCurrentEmployement
+    getIndividualCurrentEmployement,
+    modifyIndividual
 }
