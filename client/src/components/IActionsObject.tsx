9558033cac2c6653bbd02beea47fe7ce583b5986
Mohammad Baqer <baqermt@baqermt-5510.rose-hulman.edu>
2020-02-07 18:35:04 -0500

    removed IActionsObject

--- client/src/components/IActionsObject.tsx
+++
@@ -1,5 +0,0 @@
-import { Action } from "material-table";
-
-export default interface IActionsObject<RowData extends Object> {
-    actions: (Action<RowData> | ((rowData: RowData) => Action<RowData>))[];
-}
