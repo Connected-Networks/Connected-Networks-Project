c4493a72a4defc24cc84160554eb4ffd4077bf22
Mohammad Baqer <baqermt@baqermt-5510.rose-hulman.edu>
2020-02-20 14:52:50 -0500

    added email and password fields for login page

--- client/src/LoginPage.tsx
+++ client/src/LoginPage.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { RouteComponentProps, useHistory } from "react-router-dom";
+import styled from "styled-components";
+import { TextField, Button } from "@material-ui/core";
 
 interface LoginPageProps {
   confirmAuth: Function;
@@ -8,13 +10,37 @@ interface LoginPageProps {
 export default function LoginPage(props: LoginPageProps) {
   const history = useHistory();
   return (
-    <button
-      onClick={() => {
-        props.confirmAuth();
-        history.push("/");
-      }}
-    >
-      Authenticate
-    </button>
+    <Container>
+      <FormContainer>
+        <TextField label="Email" variant="outlined" />
+        <TextField label="Password" variant="outlined" />
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => {
+            props.confirmAuth();
+            history.push("/");
+          }}
+        >
+          Connect
+        </Button>
+      </FormContainer>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FormContainer = styled.div`
+  height: 30vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`;
