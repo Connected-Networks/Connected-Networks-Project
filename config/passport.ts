26ab4a99698e0fb9acf79170a5442498f008a9fb
Mohammad Baqer <baqermt@baqermt-5510.rose-hulman.edu>
2020-02-27 17:48:54 -0500

    added some todos for Aaron to look at

--- config/passport.ts
+++ config/passport.ts
@@ -3,6 +3,7 @@ const LocalStrategy = require("passport-local").Strategy;
 
 const strategy = new LocalStrategy((username, password, done) => {
   //Temp until we integrate with the database
+  //Todo for Aaron: Authenticate user here. Should check if username and passwords are matching with the ones in the database.
   if (username === "user1" && password === "123") {
     const user = {
       _id: 0,
@@ -27,6 +28,9 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((user, done) => {
+  //Todo for Aaron: Simply get the user with the matching id from the database and return it with done(null, user);
+  //It is safe to assume that the user exists because the check process will be always be called before calling this method.
+  //However, I might be wrong.
   if (user._id === 0) {
     const user1 = {
       _id: 0,
