4393587388bfb10b36dc7ebedb48aaa254c7091a
Mohammad Baqer <baqermt@baqermt-5510.rose-hulman.edu>
2020-03-21 15:23:09 -0400

    tested that signup checks if password is valid

--- __tests__/Auth.test.ts
+++ __tests__/Auth.test.ts
@@ -9,11 +9,12 @@ describe("Signup", () => {
   const request = supertest(app);
 
   it("should return 406 if email is invalid", async () => {
-    const mockEmail = "";
-    const mockPassword = "";
-    const mockUsername = "";
+    const mockEmail = "mock@test.com";
+    const mockPassword = "1234567";
+    const mockUsername = "TestUser";
 
     jest.spyOn(BackendProcessing.prototype, "emailIsValid").mockReturnValue(false);
+    jest.spyOn(BackendProcessing.prototype, "passwordIsValid").mockReturnValue(true);
 
     const response = await request.post("/signup").send({ email: mockEmail, username: mockUsername, password: mockPassword });
 
@@ -23,5 +24,23 @@ describe("Signup", () => {
 
     expect(mockedBackendProcessingInstance.emailIsValid).toBeCalledTimes(1);
     expect(mockedBackendProcessingInstance.emailIsValid).toBeCalledWith(mockEmail);
+  });
+
+  it("should return 406 if password is invalid", async () => {
+    const mockEmail = "mock@test.com";
+    const mockPassword = "1234567";
+    const mockUsername = "TestUser";
+
+    jest.spyOn(BackendProcessing.prototype, "emailIsValid").mockReturnValue(true);
+    jest.spyOn(BackendProcessing.prototype, "passwordIsValid").mockReturnValue(false);
+
+    const response = await request.post("/signup").send({ email: mockEmail, username: mockUsername, password: mockPassword });
+
+    const mockedBackendProcessingInstance = mockedBackendProcessing.mock.instances[0];
+
+    expect(response.status).toBe(406);
+
+    expect(mockedBackendProcessingInstance.passwordIsValid).toBeCalledTimes(1);
+    expect(mockedBackendProcessingInstance.passwordIsValid).toBeCalledWith(mockEmail);
   });
 });
