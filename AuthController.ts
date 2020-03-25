76695b452822e6c417328c2a246bc024d5863e1c
Mohammad Baqer <baqermt@baqermt-5510.rose-hulman.edu>
2020-03-24 23:48:43 -0400

    extracted the signup logic to AuthController

---
+++ AuthController.ts
@@ -0,0 +1,30 @@
+import BackendProcessing from "./backend-processing";
+
+export default class AuthController {
+  static signup = async (req, res) => {
+    const be = new BackendProcessing();
+    const { email, username, password } = req.body;
+
+    if (!be.emailIsValid(email) || !be.passwordIsValid(password)) {
+      res.sendStatus(406);
+      return;
+    }
+
+    if (await be.emailIsTaken(email)) {
+      res.sendStatus(409); //TODO: Add error message
+      return;
+    }
+
+    if (await be.usernameIsTaken(username)) {
+      res.sendStatus(409); //TODO: Add a different error message
+      return;
+    }
+
+    be.insertUser(email, username, password)
+      .then(() => res.sendStatus(200))
+      .catch(error => {
+        console.log(error);
+        res.sendStatus(500);
+      });
+  };
+}
