25613d35aa3e5edf701a90be5f027c9f79780749
Mohammad Baqer <baqermt@baqermt-5510.rose-hulman.edu>
2020-03-26 00:22:17 -0400

    added a couple of more cases for emailIsValid

--- __tests__/Auth/Signup/HelperFunctions.test.ts
+++ __tests__/Auth/Signup/HelperFunctions.test.ts
@@ -9,8 +9,38 @@ describe("emailIsValid", () => {
     expect(AuthController.emailIsValid(testEmail)).toBe(true);
   });
 
-  it("should rejects emails in the form of something@something", () => {
+  it("should accept emails in the form of some.thing@something.com", () => {
+    const testEmail = "some.thing@something.com";
+    expect(AuthController.emailIsValid(testEmail)).toBe(true);
+  });
+
+  it("should accept emails in the form of something@some.thing.com", () => {
+    const testEmail = "something@some.thing.com";
+    expect(AuthController.emailIsValid(testEmail)).toBe(true);
+  });
+
+  it("should accept emails in the form of something@something.something", () => {
+    const testEmail = "something@something.something";
+    expect(AuthController.emailIsValid(testEmail)).toBe(true);
+  });
+
+  it("should reject emails in the form of something@something", () => {
     const testEmail = "something@something";
+    expect(AuthController.emailIsValid(testEmail)).toBe(false);
+  });
+
+  it("should reject emails in the form of @something.com", () => {
+    const testEmail = "@something.com";
+    expect(AuthController.emailIsValid(testEmail)).toBe(false);
+  });
+
+  it("should reject emails in the form of something.com", () => {
+    const testEmail = "something.com";
+    expect(AuthController.emailIsValid(testEmail)).toBe(false);
+  });
+
+  it("should reject empty emails", () => {
+    const testEmail = "";
     expect(AuthController.emailIsValid(testEmail)).toBe(false);
   });
 });
